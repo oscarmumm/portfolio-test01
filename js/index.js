c3edbe39d2ddcb44a72533b505f2342fb172f7e4
Oscar Mumm <oscarmumm@gmail.com>
2023-03-29 00:04:43 -0300

    font size fixed

--- js/index.js
+++ js/index.js
@@ -37,47 +37,47 @@ navLink.forEach((element) => {
 
 //---------- this is for typing animation ----------
 
-const node1 = d.getElementById('node1');
-const node2 = d.getElementById('node2');
-const node3 = d.getElementById('node3');
-const text1 = `function OscarMumm () {`;
-const text2 = ` return 'Front-end Web Developer';`;
-const text3 = `};`;
-let i = 0;
-let j = 0;
-let k = 0;
-function typing1() {
-  if (i < text1.length) {
-    node1.innerHTML += `${text1.charAt(i)}`;
-    i++;
-    setTimeout(typing1, 50);
-  }
-}
-function typing2() {
-  if (j < text2.length) {
-    node2.innerHTML += `${text2.charAt(j)}`;
-    j++;
-    setTimeout(typing2, 50);
-  }
-}
-function typing3() {
-  if (k < text3.length) {
-    node3.innerHTML += `${text3.charAt(k)}`;
-    k++;
-    setTimeout(typing3, 50);
-  }
-}
+// const node1 = d.getElementById('node1');
+// const node2 = d.getElementById('node2');
+// const node3 = d.getElementById('node3');
+// const text1 = `function OscarMumm () {`;
+// const text2 = ` return 'Front-end Web Developer';`;
+// const text3 = `};`;
+// let i = 0;
+// let j = 0;
+// let k = 0;
+// function typing1() {
+//   if (i < text1.length) {
+//     node1.innerHTML += `${text1.charAt(i)}`;
+//     i++;
+//     setTimeout(typing1, 50);
+//   }
+// }
+// function typing2() {
+//   if (j < text2.length) {
+//     node2.innerHTML += `${text2.charAt(j)}`;
+//     j++;
+//     setTimeout(typing2, 50);
+//   }
+// }
+// function typing3() {
+//   if (k < text3.length) {
+//     node3.innerHTML += `${text3.charAt(k)}`;
+//     k++;
+//     setTimeout(typing3, 50);
+//   }
+// }
 
-function colorizePresentation () {
-  presentationPhase1.style.display = 'block';
-  presentationPhase2.style.display = 'none';
-}
-window.addEventListener('load', () => {
-  typing1();
-  setTimeout(typing2, 1500);
-  setTimeout(typing3, 3500);
-  setTimeout(colorizePresentation, 4200);
-});
+// function colorizePresentation () {
+//   presentationPhase1.style.display = 'block';
+//   presentationPhase2.style.display = 'none';
+// }
+// window.addEventListener('load', () => {
+//   typing1();
+//   setTimeout(typing2, 1500);
+//   setTimeout(typing3, 3500);
+//   setTimeout(colorizePresentation, 4200);
+// });
 
 
 //---------------------- OBSERVER ----------------------
