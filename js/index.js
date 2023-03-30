9b4ec0b55bd7ee99a9ab1b86f26cbfab32d92ecc
Oscar Mumm <oscarmumm@gmail.com>
2023-03-30 17:04:47 -0300

    minor fixes

--- js/index.js
+++ js/index.js
@@ -6,7 +6,10 @@ const menuIconDiv3 = d.getElementById('menu-icon-div3');
 const menuIcon = d.getElementById('menu-icon');
 
 const navBar = d.getElementById('navbar');
+const navLink = d.querySelectorAll('.nav-link');
 
+
+//------------------------------------------ CONSTS FOR OBSERVER SECTIONS
 const startSection = d.getElementById('start-section');
 const aboutSection = d.getElementById('about-section');
 const skillsSection = d.getElementById('skills-section');
@@ -14,8 +17,9 @@ const projectsSection = d.getElementById('projects-section');
 const contactSection = d.getElementById('contact-section');
 const certificationSection = d.getElementById('certifications-section');
 
-const navLink = d.querySelectorAll('.nav-link');
 
+
+//------------------------------------------ MENU ICON FUNCTION
 menuIcon.addEventListener('click', () => {
   menuIconDiv1.classList.toggle('menu-click-style1');
   menuIconDiv2.classList.toggle('menu-click-style2');
@@ -23,6 +27,8 @@ menuIcon.addEventListener('click', () => {
   navBar.classList.toggle('navbar-collapse');
 });
 
+
+//------------------------------------------ NAV LINKS FUNCTION
 navLink.forEach((element) => {
   element.addEventListener('click', () => {
     menuIconDiv1.classList.toggle('menu-click-style1');
@@ -33,45 +39,24 @@ navLink.forEach((element) => {
 })
 
 
-//---------- this is for typing animation --------
+//------------------------------------------ TYPING ANIMATION
 const node1 = d.getElementById('node1');
-const node2 = d.getElementById('node2');
-const node3 = d.getElementById('node3');
-const text1 = `{ OscarMumm :`;
-const text2 = `'Front-end Web`;
-const text3 = `Developer};`;
-let i = 0;
-let j = 0;
-let k = 0;
-function typing1() {
-  if (i < text1.length) {
-    node1.innerHTML += `${text1.charAt(i)}`;
-    i++;
-    setTimeout(typing1, 50);
+const text1 = `const OscarMumm = { 'Front-end Web Developer' };`;
+let typingIndex = 0;
+function typing() {
+  if (typingIndex < text1.length) {
+    node1.innerHTML += `${text1.charAt(typingIndex)}`;
+    typingIndex++;
+    setTimeout(typing, 75);
   }
 }
-function typing2() {
-  if (j < text2.length) {
-    node2.innerHTML += `${text2.charAt(j)}`;
-    j++;
-    setTimeout(typing2, 50);
-  }
-}
-function typing3() {
-  if (k < text3.length) {
-    node3.innerHTML += `${text3.charAt(k)}`;
-    k++;
-    setTimeout(typing3, 50);
-  }
-}
+
 window.addEventListener('load', () => {
-  typing1();
-  setTimeout(typing2, 900);
-  setTimeout(typing3, 1800);
+  typing();
 });
 
 
-//---------------------- OBSERVER ----------------------
+//-------------------------------------------- OBSERVER 
 const loadSection = (entries, observer) => {
   entries.forEach((entry) => {
     if(entry.isIntersecting){
